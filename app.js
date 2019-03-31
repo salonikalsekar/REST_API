ede0538f33f936be69efc21c889020f3eed79ac8
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-03-30 20:52:33 -0600

    cors

--- app.js
+++ app.js
@@ -7,8 +7,36 @@ const feedRoutes = require('./routes/feed');
 // app.use(bodyParser.urlencoded()); for form type data
 app.use(bodyParser.json());
 
+//setting cors
+app.use((eq, res, next) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization', );
+    next();
+});
+
 app.use('/feed', feedRoutes);
 
 app.listen(3000);
 
+
+
+
+/// requests from UI ////
+
+// const getButton = document.getElementById('get')
+// const addButton = document.getElementById('add')
+
+// getButton.addEventListener('click', () =>{
+//   fetch('http://localhost:3000/feed/posts').then(res => res.json()).then(resData => console.log(resData)).catch(err => console.log(err));
+// })
+
+// addButton.addEventListener('click', () =>{
+//   fetch('http://localhost:3000/feed/post', {
+// method: 'POST', 
+// body: JSON.stringify({ title: "haha"})
+// ,headers: {
+//         'Content-Type' : 'application/json'
+//     }}).then(res => res.json()).then(resData => console.log(resData)).catch(err => console.log(err));
+// })
 
